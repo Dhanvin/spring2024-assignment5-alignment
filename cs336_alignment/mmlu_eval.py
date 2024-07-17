10678714fb0b33684a7f00767a7736a107f55dad
Dhanvin <dhanvinm@gmail.com>
2024-07-17 12:46:05 -0700

    simplify output to file

--- cs336_alignment/mmlu_eval.py
+++ cs336_alignment/mmlu_eval.py
@@ -34,20 +34,17 @@ HfFolder.save_token("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")
 
 @dataclass
 class MmluEvalUnit:
-    prompt: str
+    subject: str
+    question: str
+    options: str
     model_response: str
     model_answer: str
-    ground_truth: str
+    ground_truth_answer: str
 
-@dataclass
-class MmluEvalSummary:
-    subject: str
-    metric_summary: str
-    deep_dive_file_path: str
     
-def generate_summary(eval_units: List[MmluEvalUnit], subject: str, deep_dive_file_path: pathlib.Path) -> MmluEvalSummary:
+def write_results(eval_units: List[MmluEvalUnit], subject: str, output_file_path: pathlib.Path):
     all_metrics = []
-    with open(deep_dive_file_path, "w") as fout:
+    with open(output_file_path, "w") as fout:
         for eval_unit in tqdm(eval_units):
             metrics = {
                 "accurate": 1.0 if eval_unit.model_answer == eval_unit.ground_truth else 0.0
@@ -57,39 +54,33 @@ def generate_summary(eval_units: List[MmluEvalUnit], subject: str, deep_dive_fil
             fout.write(
                 json.dumps(
                     {
-                        "model_prompt": eval_unit.prompt,
+                        "subject": eval_unit.subject,
+                        "question": eval_unit.question,
+                        "options": eval_unit.options,
                         "model_response": eval_unit.model_response,
+                        "model_answer": eval_unit.model_answer,
+                        "ground_truth_answer": eval_unit.ground_truth_answer,
                         "metrics": metrics,
                     }
                 )
                 + "\n"
             )
 
-    metric_summary_str = ''  
     for key in sorted(list(all_metrics[0].keys())):
         metric_mean = sum([metrics[key] for metrics in all_metrics]) / len(all_metrics)
         metric_str = f"Mean({key}): {metric_mean}\n"
-        metric_summary_str += metric_str
         logger.info(metric_str)
-    
-    return MmluEvalSummary(
-        subject= subject,
-        metric_summary=metric_summary_str,
-        deep_dive_file_path = str(deep_dive_file_path),
-    )
 
 
 class BatchPromptDispatcher():
     def __init__(self, batch_size, llm: LLM, sampling_params: SamplingParams):
         self.batch_size = batch_size
         self.prompt_responses = {}
-        self.prompt_gt = {}
         self.llm = llm
         self.sampling_params = sampling_params
     
     def add(self, prompt, ground_truth):
         self.prompt_responses[prompt] = 'NOT EXECUTED'
-        self.prompt_gt[prompt] = ground_truth
         if self.prompt_responses == self.batch_size:
             return self.query_model_and_flush()
         return None
@@ -100,29 +91,25 @@ class BatchPromptDispatcher():
 
         # Generate texts from the prompts. The output is a list of RequestOutput objects 
         # that contain the prompt, generated text, and other information.
-        if self.llm is not None: 
-            outputs = self.llm.generate(prompts, self.sampling_params)
-            # Print the outputs.
-            for output in outputs:
-                prompt = output.prompt
-                generated_text = output.outputs[0].text.strip()
-                self.prompt_responses[prompt] = generated_text
-
-        ### DEBUG ONY
-        # The expectation is that the model generates the answer, 
-        # closes the markdown code block (with ```), 
-        # and then starts the next conversation turn (with # Query:). 
-        # Thus, when we see the string # Query: we can stop response generation.
-        else: 
-            for prompt in self.prompt_responses.keys():
-                self.prompt_responses[prompt] = prompt + f"The correct answer is {self.prompt_gt[prompt]}.```"
+        outputs = self.llm.generate(prompts, self.sampling_params)
+        # Print the outputs.
+        for output in outputs:
+            prompt = output.prompt
+            generated_text = output.outputs[0].text.strip()
+            self.prompt_responses[prompt] = generated_text
 
         # Procure results
-        output = [MmluEvalUnit(prompt, resp, parse_mmlu_response(resp), self.prompt_gt[prompt]) for prompt, resp in self.prompt_responses.items()]
+        output = self.prompt_responses
         self.prompt_responses = {}
-        self.prompt_gt = {}
         return output
 
+def _to_mmlu_eval_units(prompt_responses, prompt_info) -> MmluEvalUnit:
+    return [MmluEvalUnit(subject=prompt_info[prompt][0],
+                         question=prompt_info[prompt][1],
+                         options=prompt_info[prompt][2],
+                         model_response=resp, 
+                         model_answer=parse_mmlu_response(resp),
+                         ground_truth_answer=prompt_info[prompt][3]) for prompt, resp in prompt_responses.items()]
 
 def _construct_mmlu_eval_prompt_template():
     # Get System prompt
@@ -158,14 +145,15 @@ def main(eval_dir, model_name_or_path, output_dir_str):
     # random.seed(42)
     # files = random.sample(eval_files, min(N_FILES, len(eval_files)))
 
-    # Generate examples
     def _get_subject_from_filename(filename: str):
         return '_'.join(pathlib.Path(filename).stem.split('_')[:-1])
+    
+    # Parse eval-files to generate examples, collect responses and compare with ground-truth    
     BATCH_SIZE = 32
     dispatcher = BatchPromptDispatcher(BATCH_SIZE, model, sampling_params)
-    all_results = []
-    all_summary = []
+    all_results: List[MmluEvalUnit] = []
     for file_name in tqdm(eval_files):
+        prompt_info = {} # prompt -> (subject, question, options, answer)
         file_path = eval_dir_path / file_name
         subject_str = _get_subject_from_filename(file_path.stem)
         print(f"Processing {file_path}")
@@ -174,41 +162,23 @@ def main(eval_dir, model_name_or_path, output_dir_str):
                 model_prompt = task_specific_prompt_template.format(subject = subject_str,
                                                                     question = parsed_example['question'],
                                                                     options = parsed_example['options']) 
-                batch_results = dispatcher.add(model_prompt, parsed_example['answer'])
+                prompt_info[model_prompt] = (subject_str, parsed_example['question'], parsed_example['options'], parsed_example['answer'])
+                batch_results = dispatcher.add(model_prompt)
                 if batch_results is not None:
-                    all_results += batch_results
+                    all_results += _to_mmlu_eval_units(batch_results, prompt_info)
             
             # Don't mix batches across subjects. Flush after each subject
             batch_results = dispatcher.query_and_flush()
             if batch_results is not None:
-                all_results += batch_results
+                all_results += _to_mmlu_eval_units(batch_results, prompt_info)
                     
                     
         # Write eval results to file. 
-        # Generate a summary.txt file for a) overall summary and b) per-subject stats
         output_dir_path = pathlib.Path(output_dir_str) / model_name_or_path / eval_dir_path.stem
-        # Create a dir for the model
         if not os.path.exists(output_dir_path):
             os.makedirs(output_dir_path)
-        # Create JSONL path
-        file_path = output_dir_path / file_name
-        deep_dive_file_name = str(file_path.stem) + '.jsonl'
-        all_summary.append(generate_summary(all_results, subject_str, deep_dive_file_path = output_dir_path / deep_dive_file_name ))
-    
-    
-    with open(output_dir_path / 'metric_summary.jsonl', "w") as fout:
-        for summary in all_summary:
-            fout.write(
-                json.dumps(
-                    {
-                        "subject": summary.subject,
-                        "metric_summary": summary.metric_summary,
-                        "deep_dive_file_path": summary.deep_dive_file_path,
-                    }
-                )
-                + "\n"
-            )
-
+        output_file_path = output_dir_path / 'eval_results.jsonl'
+        write_results(all_results, subject_str, output_file_path)
 
 # Create a file Parser
 if __name__ == "__main__":
